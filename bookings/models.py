2110881bd7b931c5ad502f23705fd99cc5e5a047
Arminas19 <armeska14@gmail.com>
2022-02-12 16:54:53 +0000

    started on models.py file

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Occupied"), (1, "Ordered"))
+
+class BookTable(models.Model):
+    people = models.IntegerField(max_length=5)
+    pick_date = models.DateField()

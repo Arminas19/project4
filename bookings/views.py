22a583cc37e552ea158372c8cf8b789345fdf4b0
Arminas19 <armeska14@gmail.com>
2022-03-03 16:16:35 +0000

    started on my class based view for my form

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.views import generic, View
 from .models import bookTable
 from .forms import BookTableForm
@@ -16,26 +17,24 @@ def signUp(request):
 def loggedin(request):
     return render(request, 'logged-in.html', context=None)
 
-def bookTables(request):
-    form = BookTableForm()
-    mydict = {'form': form}
-    return render(request, 'book-table.html', context=mydict)
+# def bookTables(request):
+#     form = BookTableForm()
+#     mydict = {'form': form}
+#     return render(request, 'book-table.html', context=mydict)
 
-class BookingTables(View):
-    def get(self, request, *args, **kwargs):
+class BookingTables(TemplateView):
+    template_name = 'book-tables.html'
 
-       
-
-        return render(request, 'book-table.html', context={
-            'form': form
-        },)
+    def get(self, request):
+        form = BookTableForm()
+        return render(request, 'book-table.html', context={'form': form})
 
     def post(self, request, *args, **kwargs):
+        form = BookTableForm(data=request.POST)
+        if form.is_valid():
+            form = BookTableForm()
+            
+        else:
+            form = BookTableForm()
 
-        
-
-
-        return render(request, 'book-table.html', context={
-            'form': form
-        },)
-        
+        return render(request, 'book-table.html', context={'form': form })

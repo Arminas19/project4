8835fa47e76322f3f5ecab8e65f962ad36c43db9
Arminas19 <armeska14@gmail.com>
2022-03-13 17:28:21 +0000

    added more code to my views.py file

--- bookings/views.py
+++ bookings/views.py
@@ -27,8 +27,11 @@ class BookingTables(TemplateView):
         return render(request, 'book-table.html', context={'form': form, 'form2': form2})
 
     def post(self, request, *args, **kwargs):
+        # form2 display's the newbookTable model onto the view.
+        # form is newBooking model.
         form = BookTableForm(data=request.POST)
         form2 = BookPeople(data=request.POST)
+        # Table_booked is used to show a success message after the booking is complete. 
         Table_booked = False
         if form.is_valid():
             first_name = request.POST['first_name']
@@ -38,38 +41,60 @@ class BookingTables(TemplateView):
             pick_time = request.POST['pick_time']
             errorMessage = None
             Bookings = newBooking.objects.all()
-            # if len(people) <= 5 and len(people) > 0:
-            #     TableInverntory.Tables == +1
-            #     if len(people) > 5 and len(people) <= 10:
-            #         TableInverntory.Tables == +2
-            #         if len(people) >= 10 and len(people) <=15:
-            #             TableInverntory.Tables == +3
-            #             if len(people) >=15 and len(people) <=20:
-            #                 TableInverntory.Tables == +4
-                            
-            # if TableInverntory.Tablecount > TableInverntory.Tables:
-            #     TableInverntory.Tables_available = True
-            # else:
-            #     TableInverntory.Tables_available = False
-            #     errorMessage = 'There isnt enough tables, please book a different time!.'
-
-            try:
-                already_full = newbookTable.objects.select_related('Booking').filter(Booking__pick_date = pick_date).get()
-                print(already_full)
-                if len(people) in already_full <= 100:
-                    errorMessage = 'Sorry please book a different date, we are already full!, Thank You'
-                else:
-                    pass
-            except:
-                already_full = False
             
+
+            # This code here is getting the first, last name and the date that the user has selected and checking if it exist's, 
+            # if it dose exist than dislpay the errorMessage, if not than proceed with the next validation.
             try:
                 already_booked = newBooking.objects.filter(first_name=first_name, last_name=last_name, pick_date=pick_date).get()
-                errorMessage = 'This User has already booked a table with us.'
+                print(already_booked)
+                errorMessage = 'Sorry only one booking per person per same date allowed.'
             except ObjectDoesNotExist:
                 already_booked = False
+                try:
+                    print('2nd try')
+                    already_full = newBooking.objects.filter(pick_date=pick_date)
+                    for booking in already_full:
+                        print(booking)
+                    booked_people = []
+                    for bookings in already_full:
+                        booked_people.append(bookings.people)
+                    number_of_booked = sum(booked_people)
+                    print(number_of_booked + 'this is number_of_booked')
+                    if number_of_booked <= 18:
+                        errorMessage = 'Sorry please book a different date, we are already full!, Thank You'
+                except:
+                    already_full = False
+                
+              
+            # try:
+            #     # already_full = newbookTable.objects.select_related('Booking').filter(Booking__pick_date = pick_date)
+            #     # print(already_full, 'already full')
+            #     # if len(already_full) >= 1:
+            #     #     errorMessage = 'Sorry only one booking per person per timeslot, Thank You'
+            #     #     return render(request, 'book-table.html', context={'form': form, 'form2': form2, 'Table_booked': Table_booked, 'errorMessage': errorMessage, 'Bookings': Bookings})
+            #     if: 
+            #         # Im getting the user's selected date here. im using the newbookTable model here becuase their is a foreignKey which i can use to 
+            #         # check the user's selected date and also the people queryset in the newbookTable objects.
+            #         already_full = newbookTable.objects.select_related('Booking').filter(Booking__pick_date = pick_date).get()
+            #         # Im going to get the number of people in the user's selected date here. 
+            #         print(already_full)
+            #         people = already_full.people
+            #         print(people)
+            #         print('-------------------------------------------')
+            #         # Here im checking if the number of people in that date is lower or equals to the max_number of people allowed. 
+            #     if len(people) in already_full <= 18:
+            #         # If the conditions meet, this will display the errorMessage that will prevent the user from booking. 
+            #         print(len(people))
+            #         errorMessage = 'Sorry please book a different date, we are already full!, Thank You'
+            # except:
+            #     # if no errorMessages are displayed, the booking procceds.
+            #     print(len(people))
+            #     already_full = False
+              
+        
             
-
+            # if no errorMessage are displayed than this will save both the forms.
             if not errorMessage:
                 Booking = newBooking(first_name=first_name, last_name=last_name, pick_date=pick_date, pick_time=pick_time)
                 Booking.save()
@@ -79,6 +104,7 @@ class BookingTables(TemplateView):
 
                 Table_booked = True
         else:
+            # if the errorMessage dose display this will return empty forms back to the user. 
             form = BookTableForm()
             from2 = BookPeople()
             return render(request, 'book-table.html', context={})

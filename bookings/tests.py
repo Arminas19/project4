f0146f7d8c7bca4be877c3a4abc351ac912cab3a
Arminas19 <armeska14@gmail.com>
2022-03-19 16:25:19 +0000

    added some test function's that i will work on later.

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,10 +1,23 @@
-from django.test import TestCase
 import unittest
-from bookings.views import BookingTables
+from django.test import TestCase
+#from .views import deleteBooking, BookingTables
+# to run test type: python3 bookings/test.py
+# watch python Testing 'Real World Test's' to learn how to test my python code.
 
 # Create your tests here.
 
 class TestBookingTables(unittest.TestCase):
-    pass
+
+    def test_if_BookingTables_is_returning_allBookings(self):
+        pass
+
+    def test_if_deleteBooking_is_getting_bookings_id(self):
+        pass
+
+    def test_if_BookingTables_throws_an_errorMessage_when_an_error_occures(self):
+        pass
+
+    def test_if_BookingTables_is_setting_Table_booked_to_True(self):
+        pass
 
 unittest.main()

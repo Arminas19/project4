9619b4d08c54c287d4ce10edbfa5ce554dcced99
Arminas19 <armeska14@gmail.com>
2022-04-21 10:35:24 +0000

    test

--- bookings/urls.py
+++ bookings/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
    path('logged-in.html', views.loggedin, name='logged-in'),
    path('book-table.html', views.BookingTables.as_view(), name='book-table'),
    path('edit-booking/<booking_id>', views.edit_bookings, name='edit_booking'),
-   path('delete/<booking_id>/<booking_id>', views.deleteBooking, name='delete_booking'),
+   path('delete/<booking_id>', views.deleteBooking, name='delete_booking'),
    path('view-bookings.html', views.view_bookings, name='view_bookings'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -96,12 +96,9 @@ class BookingTables(CreateView):
 
 def edit_bookings(request, booking_id):
     booking = newBooking.objects.get(id=booking_id)
-    booking2 = newbookTable.objects.get(id=booking_id)
     form = BookTableForm(instance=booking)
-    form2 = BookPeople(instance=booking2)
     context = {
-        'form': form,
-        'form2': form2
+        'form': form
     }
     return render(request, 'edit-booking.html', context)
     
